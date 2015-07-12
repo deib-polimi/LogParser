90ebc95407beae6a3c34cc2de55ca8bab48f36ad
Eugenio Gianniti <egianniti@gmail.com>
2015-07-12 18:51:38 +0200

    Refactor Status class hierarchy and use pattern matching

--- src/main/scala/parser/Status.scala
+++ src/main/scala/parser/Status.scala
@@ -10,50 +10,49 @@ import java.util.Locale
  * @author Alessandro
  *
  */
-abstract class Status(values : Map[String, Long]) {
+case class Waiting (values : Map[String, Long]) extends Status (values)
+object Start extends Waiting (Map ())
+case class Init (name : String, values : Map[String, Long]) extends Status (values)
+case class Started (name : String, startTime : Long, endTime : Long,
+	                  values : Map[String, Long]) extends Status(values)
 
-    protected val dateRegexp = """\d+-\d+-\d+ \d+:\d+:\d+,\d+""".r;
+abstract class Status (values : Map[String, Long]) {
 
-    protected def parseTime(input : String) =
-	    new SimpleDateFormat("yyyy-MM-dd hh:mm:ss,SSS", Locale.ENGLISH).parse(input).getTime;
+  protected def parseTime(input : String) =
+	  new SimpleDateFormat ("yyyy-MM-dd hh:mm:ss,SSS", Locale.ENGLISH).parse (input).getTime
 
-    def next(line : String) : Status;
+  def next (line : String) : Status = this match {
 
-    def records = values;
-}
-
-case class Waiting(values : Map[String, Long]) extends Status(values){
-
-    private val init = """(?<=LogType:syslog_)attempt_\d+_\d+_\d+_\d+_\d+_\d+""".r;
-
-    override def next(line : String) : Status = {
-	    val name = init.findFirstIn(line);
-	    if(name.isDefined) Init(name.get, values);
-	    else this;
+    case Waiting (values) => {
+	    val name = StatusRegex.init findFirstIn line
+	    if (name.isDefined) Init (name.get, values)
+	    else this
     }
-}
 
-object Start extends Waiting(Map());
-
-case class Init(name : String, values : Map[String, Long]) extends Status(values){
-
-    override def next(line : String) : Status = {
-	    val time = dateRegexp.findFirstIn(line);
-	    if(time.isDefined) Started(name, parseTime(time.get), parseTime(time.get), values);
-	    else this;
+    case Init (name, values) => {
+	    val when = StatusRegex.date findFirstIn line
+    	if (when.isDefined) {
+        val time = parseTime (when.get)
+        Started (name, time, time, values)
+      }
+	    else this
     }
-}
 
-case class Started(name : String, startTime : Long, endTime : Long, values : Map[String, Long])
-extends Status(values){
-
-    override def next(line : String) : Status = {
+    case Started (name, start, end, values) => {
 	    def updateTime = {
-		    val time = dateRegexp.findFirstIn(line);
-		    if(time.isDefined) Started(name, startTime, parseTime(time.get), values);
-		    else this;
+		    val time = StatusRegex.date findFirstIn line
+		    if (time.isDefined) Started (name, start, parseTime (time.get), values)
+		    else this
 	    }
-	    if(line.isEmpty) Waiting(values + (name -> (endTime -startTime)));
-	    else updateTime;
+	    if (line.isEmpty) Waiting (values + (name -> (end - start)))
+	    else updateTime
     }
+  }
+
+  def records = values
+}
+
+object StatusRegex {
+  val init = """(?<=LogType:syslog_)attempt_\d+_\d+_\d+_\d+_\d+_\d+""".r
+  val date = """\d+-\d+-\d+ \d+:\d+:\d+,\d+""".r
 }
