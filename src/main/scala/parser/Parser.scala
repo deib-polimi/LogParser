bfb5303b9f5a5763854135e6b4cea7027aa1da21
Eugenio Gianniti <eugenio.gianniti@polimi.it>
2015-10-15 12:37:45 +0200

    Fix indentation

--- src/main/scala/parser/Parser.scala
+++ src/main/scala/parser/Parser.scala
@@ -3,10 +3,9 @@
  */
 package parser
 
-import scala.io.Source
+import java.io.{File, FileWriter}
 
-import java.io.File
-import java.io.FileWriter
+import scala.io.Source
 
 /**
  * @author Alessandro
@@ -14,96 +13,96 @@ import java.io.FileWriter
  */
 object Parser {
 
-    def apply (filename : String): (StartEnd, Durations, StartEnd, Durations,
-                                    Sequence, VertexListOfTasks, TaskNodes,
-                                    TaskContainers) = {
-	    println("Starting " + filename)
-	    val lines = Source.fromFile(filename).getLines();
+  def apply (filename : String): (StartEnd, Durations, StartEnd, Durations,
+    Sequence, VertexListOfTasks, TaskNodes,
+    TaskContainers) = {
+    println("Starting " + filename)
+    val lines = Source.fromFile(filename).getLines();
 
-	    val start = System.currentTimeMillis();
-	    val finalStatus = lines.foldLeft(Start : Status)(_ next _);
-	    val stop = System.currentTimeMillis();
-	    val i = new File(filename).length;
-	    println("Finished in " + ((stop - start).toDouble / 1000) + " s");
-	    val ratio = i.toLong * 1000 / (stop-start);
-	    println(i + ": " + ratio/1000 + "KB/s")
-	    (StartEnd (finalStatus.times), Durations (finalStatus.times),
-       StartEnd (finalStatus.shuffleTimes), Durations (finalStatus.shuffleTimes),
-       Sequence (finalStatus.vertices),
-       VertexListOfTasks (finalStatus.taskToVertices, finalStatus.taskOrder),
-       TaskNodes (finalStatus.containerToNodes, finalStatus.taskToContainers,
-                  finalStatus.taskOrder),
-       TaskContainers (finalStatus.taskToContainers, finalStatus.taskOrder))
-    }
+    val start = System.currentTimeMillis();
+    val finalStatus = lines.foldLeft(Start : Status)(_ next _);
+    val stop = System.currentTimeMillis();
+    val i = new File(filename).length;
+    println("Finished in " + ((stop - start).toDouble / 1000) + " s");
+    val ratio = i.toLong * 1000 / (stop-start);
+    println(i + ": " + ratio/1000 + "KB/s")
+    (StartEnd (finalStatus.times), Durations (finalStatus.times),
+      StartEnd (finalStatus.shuffleTimes), Durations (finalStatus.shuffleTimes),
+      Sequence (finalStatus.vertices),
+      VertexListOfTasks (finalStatus.taskToVertices, finalStatus.taskOrder),
+      TaskNodes (finalStatus.containerToNodes, finalStatus.taskToContainers,
+        finalStatus.taskOrder),
+      TaskContainers (finalStatus.taskToContainers, finalStatus.taskOrder))
+  }
 
-    def apply (files : Seq[String]): (String, String, String, String, String,
-                                      String, String, String) = {
-	    val (taskStartEnds, taskDurations, shuffleStartEnds, shuffleDurations,
-           vertices, listsOfTasks, taskNodes, taskContainers) =
-        files.map (Parser (_))
+  def apply (files : Seq[String]): (String, String, String, String, String,
+    String, String, String) = {
+    val (taskStartEnds, taskDurations, shuffleStartEnds, shuffleDurations,
+    vertices, listsOfTasks, taskNodes, taskContainers) =
+      files.map (Parser (_))
         .foldLeft ((Seq (): Seq[StartEnd], Seq (): Seq[Durations],
-                    Seq (): Seq[StartEnd], Seq (): Seq[Durations],
-                    Seq (): Seq[Sequence], Seq (): Seq[VertexListOfTasks],
-                    Seq (): Seq[TaskNodes], Seq (): Seq[TaskContainers]))
+          Seq (): Seq[StartEnd], Seq (): Seq[Durations],
+          Seq (): Seq[Sequence], Seq (): Seq[VertexListOfTasks],
+          Seq (): Seq[TaskNodes], Seq (): Seq[TaskContainers]))
         {(lists, tuple) => (lists._1 :+ tuple._1, lists._2 :+ tuple._2,
-                            lists._3 :+ tuple._3, lists._4 :+ tuple._4,
-                            lists._5 :+ tuple._5, lists._6 :+ tuple._6,
-                            lists._7 :+ tuple._7, lists._8 :+ tuple._8)}
-	    (taskStartEnds mkString "\n\n", taskDurations mkString "\n\n",
-       shuffleStartEnds mkString "\n\n", shuffleDurations mkString "\n\n",
-       vertices mkString "\n", listsOfTasks mkString "\n\n",
-       taskNodes mkString "\n\n", taskContainers mkString "\n\n")
-    }
+          lists._3 :+ tuple._3, lists._4 :+ tuple._4,
+          lists._5 :+ tuple._5, lists._6 :+ tuple._6,
+          lists._7 :+ tuple._7, lists._8 :+ tuple._8)}
+    (taskStartEnds mkString "\n\n", taskDurations mkString "\n\n",
+      shuffleStartEnds mkString "\n\n", shuffleDurations mkString "\n\n",
+      vertices mkString "\n", listsOfTasks mkString "\n\n",
+      taskNodes mkString "\n\n", taskContainers mkString "\n\n")
+  }
 
-    def parse (path : String): Unit = {
-	    val sourceDir = new File (path).getAbsoluteFile
-      val dataDir = new File (sourceDir, "data")
-      dataDir.mkdir
+  def parse (path : String): Unit = {
+    val sourceDir = new File (path).getAbsoluteFile
+    val dataDir = new File (sourceDir, "data")
+    dataDir.mkdir
 
-      val appDurationIn = new File (sourceDir, "appDuration.txt")
-	    val appDurationOut = new File (dataDir, "appDuration.txt")
-      copyFile(appDurationIn, appDurationOut)
+    val appDurationIn = new File (sourceDir, "appDuration.txt")
+    val appDurationOut = new File (dataDir, "appDuration.txt")
+    copyFile(appDurationIn, appDurationOut)
 
-      val inputFiles = sourceDir.listFiles ().sortBy (_.getName)
-        .map (_.getPath).filter (_.endsWith (".AMLOG.txt")).toSeq
-      val (taskStartEndContent, taskDurationContent, shuffleStartEndContent,
-           shuffleDurationContent, verticesContent, listOfTasksContent,
-           taskNodesContent, taskContainersContent) = Parser (inputFiles)
-      writeToFile (taskStartEndContent, new File (dataDir, "taskStartEnd.txt"))
-	    writeToFile (taskDurationContent, new File (dataDir, "taskDurationLO.txt"))
-      writeToFile (shuffleStartEndContent, new File (dataDir, "shuffleStartEnd.txt"))
-      writeToFile (shuffleDurationContent, new File (dataDir, "shuffleDurationLO.txt"))
-      writeToFile (verticesContent, new File (dataDir, "vertexOrder.txt"))
-      writeToFile (listOfTasksContent, new File (dataDir, "vertexLtask.txt"))
-      writeToFile (taskNodesContent, new File (dataDir, "taskNode.txt"))
-      writeToFile (taskContainersContent, new File (dataDir, "taskContainer.txt"))
-    }
+    val inputFiles = sourceDir.listFiles ().sortBy (_.getName)
+      .map (_.getPath).filter (_.endsWith (".AMLOG.txt")).toSeq
+    val (taskStartEndContent, taskDurationContent, shuffleStartEndContent,
+    shuffleDurationContent, verticesContent, listOfTasksContent,
+    taskNodesContent, taskContainersContent) = Parser (inputFiles)
+    writeToFile (taskStartEndContent, new File (dataDir, "taskStartEnd.txt"))
+    writeToFile (taskDurationContent, new File (dataDir, "taskDurationLO.txt"))
+    writeToFile (shuffleStartEndContent, new File (dataDir, "shuffleStartEnd.txt"))
+    writeToFile (shuffleDurationContent, new File (dataDir, "shuffleDurationLO.txt"))
+    writeToFile (verticesContent, new File (dataDir, "vertexOrder.txt"))
+    writeToFile (listOfTasksContent, new File (dataDir, "vertexLtask.txt"))
+    writeToFile (taskNodesContent, new File (dataDir, "taskNode.txt"))
+    writeToFile (taskContainersContent, new File (dataDir, "taskContainer.txt"))
+  }
 
-    protected def writeToFile (content: String, file: File): Unit = {
-      if (! content.isEmpty) {
-        file.delete
-        val out = new FileWriter (file)
-        out write {
-          if (content.last == '\n') content
-          else content :+ '\n'
-        }
-        out.flush
-        out.close
+  protected def writeToFile (content: String, file: File): Unit = {
+    if (! content.isEmpty) {
+      file.delete
+      val out = new FileWriter (file)
+      out write {
+        if (content.last == '\n') content
+        else content :+ '\n'
       }
+      out.flush
+      out.close
     }
+  }
 
-    protected def copyFile (from: File, to: File): Unit = {
-      val content = Source.fromFile (from).mkString
-      to.delete
-      val copy = new FileWriter(to)
-      copy.write(content)
-      copy.flush()
-      copy.close()
-    }
+  protected def copyFile (from: File, to: File): Unit = {
+    val content = Source.fromFile (from).mkString
+    to.delete
+    val copy = new FileWriter(to)
+    copy.write(content)
+    copy.flush()
+    copy.close()
+  }
 
-    def main(args: Array[String]): Unit = {
-      var directory = "/workspace/RC/5_80_R3/fetched/R3/"
-      if (args.length > 0) directory = args(0)
-	    Parser parse directory
-    }
+  def main(args: Array[String]): Unit = {
+    var directory = "/workspace/RC/5_80_R3/fetched/R3/"
+    if (args.length > 0) directory = args(0)
+    Parser parse directory
+  }
 }
--- src/main/scala/parser/Status.scala
+++ src/main/scala/parser/Status.scala
@@ -5,7 +5,6 @@ package parser
 
 import java.text.SimpleDateFormat
 import java.util.Locale
-import java.util.NoSuchElementException
 
 /**
  * @author Alessandro
@@ -26,7 +25,7 @@ case class Init (name : String, tm : Map[String, (Long, Long)], vs : Seq[String]
   extends Status (tm, vs, tv, tc, to, cn, stm)
 
 case class Started (name : String, startTime : Long, endTime : Long,
-	                  tm : Map[String, (Long, Long)], vs : Seq[String],
+                    tm : Map[String, (Long, Long)], vs : Seq[String],
                     tv : Map[String, Seq[String]], tc : Map[String, String],
                     to : Seq[String], cn : Map[String, String],
                     stm : Map[String, (Long, Long)])
@@ -53,128 +52,128 @@ abstract class Status (tm : Map[String, (Long, Long)], vs : Seq[String],
   def shuffleTimes: Map[String, (Long, Long)] = stm
 
   protected def parseTime(input : String) =
-	  new SimpleDateFormat ("yyyy-MM-dd HH:mm:ss,SSS", Locale.ENGLISH).parse (input).getTime
+    new SimpleDateFormat ("yyyy-MM-dd HH:mm:ss,SSS", Locale.ENGLISH).parse (input).getTime
 
   def next (line : String) : Status = {
 
-	  def nextVertices: Seq[String] = StatusRegex.vertex findFirstMatchIn line match {
-	    case Some (m) => vertices :+ (m group 1)
-	    case None => vertices
+    def nextVertices: Seq[String] = StatusRegex.vertex findFirstMatchIn line match {
+      case Some (m) => vertices :+ (m group 1)
+      case None => vertices
     }
 
     def nextTaskToVertices: Map[String, Seq[String]] =
       StatusRegex.taskToVertex findFirstMatchIn line match {
-      case Some (m) => {
-        val (vertex, task) = (m group 1, m group 2)
-        if (taskToVertices contains vertex) {
-          val nextSeq = taskToVertices (vertex) :+ task
-          taskToVertices + (vertex -> nextSeq)
+        case Some (m) => {
+          val (vertex, task) = (m group 1, m group 2)
+          if (taskToVertices contains vertex) {
+            val nextSeq = taskToVertices (vertex) :+ task
+            taskToVertices + (vertex -> nextSeq)
+          }
+          else {
+            val nextSeq = Seq (task)
+            taskToVertices + (vertex -> nextSeq)
+          }
         }
-        else {
-          val nextSeq = Seq (task)
-          taskToVertices + (vertex -> nextSeq)
-        }
+        case None => taskToVertices
       }
-      case None => taskToVertices
-    }
 
     def nextTaskToContainers: Map[String, String] =
       StatusRegex.taskToContainer findFirstMatchIn line match {
-      case Some (m) => {
-        val (task, container) = (m group 1, m group 2)
-        taskToContainers + (task -> container)
+        case Some (m) => {
+          val (task, container) = (m group 1, m group 2)
+          taskToContainers + (task -> container)
+        }
+        case None => taskToContainers
       }
-      case None => taskToContainers
-    }
 
     def nextTaskOrder: Seq[String] =
       StatusRegex.taskToContainer findFirstMatchIn line match {
-      case Some (m) => taskOrder :+ (m group 1)
-      case None => taskOrder
-    }
+        case Some (m) => taskOrder :+ (m group 1)
+        case None => taskOrder
+      }
 
     def nextContainerToNodes: Map[String, String] =
       StatusRegex.receivedContainer findFirstMatchIn line match {
-      case Some (m) => {
-        val (container, node) = (m group 1, m group 2)
-        containerToNodes + (container -> node)
+        case Some (m) => {
+          val (container, node) = (m group 1, m group 2)
+          containerToNodes + (container -> node)
+        }
+        case None => containerToNodes
       }
-      case None => containerToNodes
-    }
 
     this match {
       case Waiting (_, _, _, _, _, _, _) => {
         val name = StatusRegex.init findFirstIn line
         if (name.isDefined) Init (name.get, times, nextVertices, nextTaskToVertices,
-                                  nextTaskToContainers, nextTaskOrder,
-                                  nextContainerToNodes, shuffleTimes)
+          nextTaskToContainers, nextTaskOrder,
+          nextContainerToNodes, shuffleTimes)
         else Waiting (times, nextVertices, nextTaskToVertices,
-                      nextTaskToContainers, nextTaskOrder,
-                      nextContainerToNodes, shuffleTimes)
+          nextTaskToContainers, nextTaskOrder,
+          nextContainerToNodes, shuffleTimes)
       }
 
       case Init (name, _, _, _, _, _, _, _) => {
         val when = StatusRegex.date findFirstIn line
         if (when.isDefined) {
-	        val time = parseTime (when.get)
-	        Started (name, time, time, times, nextVertices, nextTaskToVertices,
-                   nextTaskToContainers, nextTaskOrder,
-                   nextContainerToNodes, shuffleTimes)
+          val time = parseTime (when.get)
+          Started (name, time, time, times, nextVertices, nextTaskToVertices,
+            nextTaskToContainers, nextTaskOrder,
+            nextContainerToNodes, shuffleTimes)
         }
         else Init (name, times, nextVertices, nextTaskToVertices,
-                   nextTaskToContainers, nextTaskOrder,
-                   nextContainerToNodes, shuffleTimes)
+          nextTaskToContainers, nextTaskOrder,
+          nextContainerToNodes, shuffleTimes)
       }
 
       case Started (name, start, end, _, _, _, _, _, _, _) => {
-	      def lookForShuffle = {
-		      def updateTime = {
-			      val time = StatusRegex.date findFirstIn line
-				    if (time.isDefined) Started (name, start, parseTime (time.get),
-					                               times, nextVertices, nextTaskToVertices,
-					                               nextTaskToContainers, nextTaskOrder,
-					                               nextContainerToNodes, shuffleTimes)
-					  else Started (name, start, end, times, nextVertices,
-                          nextTaskToVertices, nextTaskToContainers,
-                          nextTaskOrder, nextContainerToNodes, shuffleTimes)
-		      }
+        def lookForShuffle = {
+          def updateTime = {
+            val time = StatusRegex.date findFirstIn line
+            if (time.isDefined) Started (name, start, parseTime (time.get),
+              times, nextVertices, nextTaskToVertices,
+              nextTaskToContainers, nextTaskOrder,
+              nextContainerToNodes, shuffleTimes)
+            else Started (name, start, end, times, nextVertices,
+              nextTaskToVertices, nextTaskToContainers,
+              nextTaskOrder, nextContainerToNodes, shuffleTimes)
+          }
           StatusRegex.startingShuffle findFirstMatchIn line match {
             case Some (_) => {
               val time = parseTime ((StatusRegex.date findFirstIn line).get)
               Shuffling (name, start, time, time, times, nextVertices,
-                         nextTaskToVertices, nextTaskToContainers,
-                         nextTaskOrder, nextContainerToNodes, shuffleTimes)
+                nextTaskToVertices, nextTaskToContainers,
+                nextTaskOrder, nextContainerToNodes, shuffleTimes)
             }
             case None => updateTime
           }
-	      }
+        }
         if (line.isEmpty) Waiting (times + (name -> (start, end)), nextVertices,
-                                   nextTaskToVertices, nextTaskToContainers,
-                                   nextTaskOrder, nextContainerToNodes,
-                                   shuffleTimes)
+          nextTaskToVertices, nextTaskToContainers,
+          nextTaskOrder, nextContainerToNodes,
+          shuffleTimes)
         else lookForShuffle
       }
 
       case Shuffling (name, startTask, endTask, startShuffle,
-                      _, _, _, _, _, _, _) => {
+      _, _, _, _, _, _, _) => {
         def updateTime = {
           val time = StatusRegex.date findFirstIn line
           if (time.isDefined) Shuffling (name, startTask, parseTime (time.get),
-                                         startShuffle, times, nextVertices,
-                                         nextTaskToVertices, nextTaskToContainers,
-                                         nextTaskOrder, nextContainerToNodes,
-                                         shuffleTimes)
+            startShuffle, times, nextVertices,
+            nextTaskToVertices, nextTaskToContainers,
+            nextTaskOrder, nextContainerToNodes,
+            shuffleTimes)
           else Shuffling (name, startTask, endTask, startShuffle, times,
-                          nextVertices, nextTaskToVertices, nextTaskToContainers,
-                          nextTaskOrder, nextContainerToNodes, shuffleTimes)
+            nextVertices, nextTaskToVertices, nextTaskToContainers,
+            nextTaskOrder, nextContainerToNodes, shuffleTimes)
         }
         StatusRegex.endingShuffle findFirstMatchIn line match {
           case Some (_) => {
             val time = parseTime ((StatusRegex.date findFirstIn line).get)
             Started (name, startTask, time, times, nextVertices,
-                     nextTaskToVertices, nextTaskToContainers, nextTaskOrder,
-                     nextContainerToNodes,
-                     shuffleTimes + (name -> (startShuffle, time)))
+              nextTaskToVertices, nextTaskToContainers, nextTaskOrder,
+              nextContainerToNodes,
+              shuffleTimes + (name -> (startShuffle, time)))
           }
           case None => updateTime
         }
