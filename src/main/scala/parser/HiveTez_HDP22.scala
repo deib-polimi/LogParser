951896ef2fdc2ec8150d22a58e129ac0bed4d2c5
Eugenio Gianniti <eugenio.gianniti@polimi.it>
2016-01-31 19:17:55 +0100

    Use String's r method with arguments instead of Regex constructor

--- src/main/scala/parser/HiveTez_HDP22.scala
+++ src/main/scala/parser/HiveTez_HDP22.scala
@@ -1,17 +1,15 @@
 package parser
 
-import scala.util.matching.Regex
-
 object HiveTez_HDP22 extends StatusRegex {
 
   override val init = """(?<=LogType:syslog_)attempt_\d+_\d+_\d+_\d+_\d+_\d+""".r
   override val date = """\d+-\d+-\d+ \d+:\d+:\d+,\d+""".r
   override val vertex = """Routing pending task events for vertex: vertex_\d+_\d+_\d+_\d+ \[(.+)\]""".r
-  override val taskToVertex = new Regex ("""impl.TaskAttemptImpl: remoteTaskSpec:DAGName.+VertexName: (.+), VertexParallelism.+TaskAttemptID:(attempt_[0-9]+_[0-9]+_[0-9]+_[0-9]+_[0-9]+_[0-9]+)""", "vertex", "task")
-  override val taskToContainer = new Regex ("""Assigned taskAttempt.+(attempt_[0-9]+_[0-9]+_[0-9]+_[0-9]+_[0-9]+_[0-9]+).+to container:.+(container_[0-9]+_[0-9]+_[0-9]+_[0-9]+)""", "task", "container")
-  override val receivedContainer = new Regex ("""Assigning container to task, container=Container: \[ContainerId: (container_[0-9]+_[0-9]+_[0-9]+_[0-9]+).* containerHost=(\w+)""", "container", "node")
+  override val taskToVertex = """impl.TaskAttemptImpl: remoteTaskSpec:DAGName.+VertexName: (.+), VertexParallelism.+TaskAttemptID:(attempt_[0-9]+_[0-9]+_[0-9]+_[0-9]+_[0-9]+_[0-9]+)""".r("vertex", "task")
+  override val taskToContainer = """Assigned taskAttempt.+(attempt_[0-9]+_[0-9]+_[0-9]+_[0-9]+_[0-9]+_[0-9]+).+to container:.+(container_[0-9]+_[0-9]+_[0-9]+_[0-9]+)""".r("task", "container")
+  override val receivedContainer = """Assigning container to task, container=Container: \[ContainerId: (container_[0-9]+_[0-9]+_[0-9]+_[0-9]+).* containerHost=(\w+)""".r("container", "node")
   override val startingShuffle = """orderedgrouped.Shuffle: Shuffle assigned with""".r
   override val endingShuffle = """orderedgrouped.Shuffle: Shutting down Shuffle for source""".r
-  override val shuffleBytes = new Regex ("""Final Counters :.*SHUFFLE_BYTES=([0-9]+),""", "bytes", "task")
+  override val shuffleBytes = """Final Counters :.*SHUFFLE_BYTES=([0-9]+),""".r("bytes", "task")
 
 }
--- src/main/scala/parser/HiveTez_HDP23.scala
+++ src/main/scala/parser/HiveTez_HDP23.scala
@@ -1,17 +1,15 @@
 package parser
 
-import scala.util.matching.Regex
-
 object HiveTez_HDP23 extends StatusRegex {
 
   override val init = """(?<=LogType:syslog_)attempt_\d+_\d+_\d+_\d+_\d+_\d+""".r
   override val date = """\d+-\d+-\d+ \d+:\d+:\d+,\d+""".r
   override val vertex = """Routing pending task events for vertex: vertex_\d+_\d+_\d+_\d+ \[(.+)\]""".r
-  override val taskToVertex = new Regex ("""\[Event:TASK_ATTEMPT_STARTED\]: vertexName=(.+), taskAttemptId=(attempt_[0-9]+_[0-9]+_[0-9]+_[0-9]+_[0-9]+_[0-9]+)""", "vertex", "task")
-  override val taskToContainer = new Regex ("""Container with id: (container_.+_[0-9]+_[0-9]+_[0-9]+_[0-9]+) given task: (attempt_[0-9]+_[0-9]+_[0-9]+_[0-9]+_[0-9]+_[0-9]+)""", "container", "task")
-  override val receivedContainer = new Regex ("""Assigning container to task: containerId=(container_.+_[0-9]+_[0-9]+_[0-9]+_[0-9]+), task=.* containerHost=(\w+)""", "container", "node")
+  override val taskToVertex = """\[Event:TASK_ATTEMPT_STARTED\]: vertexName=(.+), taskAttemptId=(attempt_[0-9]+_[0-9]+_[0-9]+_[0-9]+_[0-9]+_[0-9]+)""".r("vertex", "task")
+  override val taskToContainer = """Container with id: (container_.+_[0-9]+_[0-9]+_[0-9]+_[0-9]+) given task: (attempt_[0-9]+_[0-9]+_[0-9]+_[0-9]+_[0-9]+_[0-9]+)""".r("container", "task")
+  override val receivedContainer = """Assigning container to task: containerId=(container_.+_[0-9]+_[0-9]+_[0-9]+_[0-9]+), task=.* containerHost=(\w+)""".r("container", "node")
   override val startingShuffle = """Shuffle assigned with""".r
   override val endingShuffle = """Shutting down Shuffle for source""".r
-  override val shuffleBytes = new Regex ( """Final Counters for (attempt_[0-9]+_[0-9]+_[0-9]+_[0-9]+_[0-9]+_[0-9]+):.*SHUFFLE_BYTES=([0-9]+),""", "task", "bytes")
+  override val shuffleBytes = """Final Counters for (attempt_[0-9]+_[0-9]+_[0-9]+_[0-9]+_[0-9]+_[0-9]+):.*SHUFFLE_BYTES=([0-9]+),""".r("task", "bytes")
 
 }
