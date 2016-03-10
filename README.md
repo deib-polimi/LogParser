8720bb00b20bab8e8c30718147d2222cad4c240d
Eugenio Gianniti <eugenio.gianniti@polimi.it>
2016-03-10 10:05:25 +0100

    Apply Apache License version 2.0

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # Hive Log Parser
 
-This Scala project allows to parse logs obtained from Hive queries runs and extract
-information about every involved task.
+This Scala project allows to parse logs obtained from Hive queries runs
+and extract information about every involved task.
+
+Code in this repository is licensed under the
+[Apache License, version 2.0](http://www.apache.org/licenses/LICENSE-2.0).
--- src/main/scala/parser/Durations.scala
+++ src/main/scala/parser/Durations.scala
@@ -1,12 +1,23 @@
-/**
+/* Copyright 2015-2016 Alessandro Maria Rizzi
  *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package parser
 
 /**
- * @author Alessandro
- *
- */
+  * @author Alessandro
+  *
+  */
 case class Durations (values : Map[String, (Long, Long)]) {
   override def toString = values map {case (k,v) =>
     k + "\t" + (v._2 - v._1)} mkString "\n"
